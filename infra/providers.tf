da7a9cabc9bf405576d26a287833de4c02232e55
Cas Teeuwen <casteeuwen@pm.me>
2026-05-05 11:19:20 +0200

    refactor(infra): remove unused random provider from Terraform configuration

--- infra/providers.tf
+++ infra/providers.tf
@@ -6,10 +6,6 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 4.67"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.7"
-    }
   }
 
   backend "azurerm" {
