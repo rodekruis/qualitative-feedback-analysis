5b1fae236eecf6a18a81bfbc875d712283248e63
Cas Teeuwen <casteeuwen@pm.me>
2026-05-20 15:08:00 +0200

    fix(db): update down_revision to correct migration sequence

--- alembic/versions/20260519_01_add_call_id_to_llm_calls.py
+++ alembic/versions/20260519_01_add_call_id_to_llm_calls.py
@@ -24,7 +24,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "20260519_01"
-down_revision: Union[str, Sequence[str], None] = "20260429_01"
+down_revision: Union[str, Sequence[str], None] = "20260511_01"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
