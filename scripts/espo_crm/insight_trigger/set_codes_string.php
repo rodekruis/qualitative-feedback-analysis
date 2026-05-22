dfa649bb4b51bdf1f0b7bf7fb589b28e72e026d1
Cas Teeuwen <casteeuwen@pm.me>
2026-05-22 13:50:20 +0200

    fix(espo): fix set_codes_string

--- scripts/espo_crm/insight_trigger/set_codes_string.php
+++ scripts/espo_crm/insight_trigger/set_codes_string.php
@@ -6,51 +6,56 @@ $ids1 = record\findMany('CCodingLevel1', 20, 'name', 'asc');
 $nodes1 = list();
 $i = 0;
 
-while($i < array\length($ids1)(
+while ($i < array\length($ids1)) {
     $id1 = array\at($ids1, $i);
-    
-    // FIX: Use record\attribute to get the name of the specific ID from Level 1
+
     $name1 = record\attribute('CCodingLevel1', $id1, 'name');
-    ifThen(string\length($name1) == 0, $name1 = $id1);
+    ifThen($name1 == null || string\length($name1) == 0, $name1 = $id1);
 
     // Search for Level 2s that point to this Level 1
-    $ids2 = record\findMany('CCodingLevel2', 50, 'name', 'asc', 'codingLevel1Id', $id1);
+    $ids2 = record\findMany('CCodingLevel2', 50, 'name', 'asc', 'codingLevel1Id=', $id1);
     $nodes2 = list();
     $j = 0;
 
-    while($j < array\length($ids2), (
+    while ($j < array\length($ids2)) {
         $id2 = array\at($ids2, $j);
-        
-        // FIX: Use record\attribute for Level 2
+
         $name2 = record\attribute('CCodingLevel2', $id2, 'name');
-        ifThen(string\length($name2) == 0, $name2 = $id2);
+        ifThen($name2 == null || string\length($name2) == 0, $name2 = $id2);
 
         // Search for Level 3s that point to this Level 2
-        $ids3 = record\findMany('CCodingLevel3', 50, 'name', 'asc', 'codingLevel2Id', $id2);
+        $ids3 = record\findMany('CCodingLevel3', 50, 'name', 'asc', 'codingLevel2Id=', $id2);
         $nodes3 = list();
         $k = 0;
 
-        while($k < array\length($ids3), (
+        while ($k < array\length($ids3)) {
             $id3 = array\at($ids3, $k);
-            
-            // FIX: Use record\attribute for Level 3
+
             $name3 = record\attribute('CCodingLevel3', $id3, 'name');
-            ifThen(string\length($name3) == 0, $name3 = $id3);
-            
-            $nodes3 = array\push($nodes3, string\concatenate('{"name": "', $name3, '", "children": []}'));
-            $k = $k + 1
-        ));
+            ifThen($name3 == null || string\length($name3) == 0, $name3 = $id3);
 
-        $nodes2 = array\push($nodes2, string\concatenate(
-            '{"name": "', $name2, '", "children": [', array\join($nodes3, ','), ']}'
-        ));
-        $j = $j + 1
-    ));
+            $node3 = object\create();
+            $node3['name'] = $name3;
+            $node3['children'] = list();
 
-    $nodes1 = array\push($nodes1, string\concatenate(
-        '{"name": "', $name1, '", "children": [', array\join($nodes2, ','), ']}'
-    ));
-    $i = $i + 1
-));
+            $nodes3 = array\push($nodes3, json\encode($node3));
+            $k = $k + 1;
+        }
+
+        $node2 = object\create();
+        $node2['name'] = $name2;
+        $node2['children'] = json\retrieve(string\concatenate('[', array\join($nodes3, ','), ']'));
+
+        $nodes2 = array\push($nodes2, json\encode($node2));
+        $j = $j + 1;
+    }
+
+    $node1 = object\create();
+    $node1['name'] = $name1;
+    $node1['children'] = json\retrieve(string\concatenate('[', array\join($nodes2, ','), ']'));
+
+    $nodes1 = array\push($nodes1, json\encode($node1));
+    $i = $i + 1;
+}
 
 $$codesString = string\concatenate('[', array\join($nodes1, ','), ']');
