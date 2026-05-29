e5b4da6a945c8b918a9247ccefbef8345dacec31
Cas Teeuwen <casteeuwen@pm.me>
2026-05-29 13:22:30 +0200

    feat(espo): add select_endpoint.php for dynamic endpoint selection

---
+++ scripts/espo_crm/insight_trigger/select_endpoint.php
@@ -0,0 +1,11 @@
+<?php // Do not copy this tag into EspoCRM
+// This selects the endpoint to hit based on the method chosen in the UI
+
+$selected_method = record\attribute('CInsight', id, 'method');
+
+if ($selected_method == "code") {$$endpoint = "/v1/assign_codes";} 
+else if ($selected_method == "summarize_aggregate") {$$endpoint = "/v1/summarize-aggregate";}
+else if ($selected_method == "summarize_per_item") {$$endpoint = "/v1/summarize";}
+else if ($selected_method == "analyze") {$$endpoint = "/v1/analyze";}
+else {$$endpoint = "/v1/summarize";}
+
