4ee9c1c1cbc417ed5fad3bb9c5dccc1565b93924
Cas Teeuwen <casteeuwen@pm.me>
2026-05-29 11:06:40 +0200

    feat(espo): add final elements to motherPayload

--- scripts/espo_crm/insight_trigger/combine_mother_payload.php
+++ scripts/espo_crm/insight_trigger/combine_mother_payload.php
@@ -1,9 +1,18 @@
 <?php // Do not copy this tag into EspoCRM
 // This creates a "mother" payload that can be used to hit any of the inference routes
 
+// TODO: use fields from UI
+$outputLanguage = "English";
+$prompt = "No prompt provided";
+
 $motherPayload = string\concatenate(
     '{',
     '"feedback_records": ', $$recordsString, ', ',
-    '"coding_framework": ', $$codesString, '',
+    '"coding_framework": ', $$codesString, ', ',
+    '"anonymize": true, ',
+    '"prompt": "', $prompt, '", ',
+    '"output_language": "', $language, '", ',
+    '"max_codes": 10, ',
+    '"confidence_threshold": 0.7',
     '}'
 );
