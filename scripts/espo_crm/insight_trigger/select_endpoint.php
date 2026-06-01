a9307cb1de734289add404bca3bf9e68f66c55b1
Cas Teeuwen <casteeuwen@pm.me>
2026-06-01 09:52:08 +0200

    feat(espo): remove 'code' endpoint selection from method logic for insight generation

--- scripts/espo_crm/insight_trigger/select_endpoint.php
+++ scripts/espo_crm/insight_trigger/select_endpoint.php
@@ -3,8 +3,7 @@
 
 $$selectedMethod = record\attribute('CInsight', id, 'method');
 
-if ($$selectedMethod == "code") {$$endpoint = "/v1/assign-codes";} 
-else if ($$selectedMethod == "summarize_aggregate") {$$endpoint = "/v1/summarize-aggregate";}
+if ($$selectedMethod == "summarize_aggregate") {$$endpoint = "/v1/summarize-aggregate";}
 else if ($$selectedMethod == "summarize_per_item") {$$endpoint = "/v1/summarize";}
 else if ($$selectedMethod == "analyze") {$$endpoint = "/v1/analyze";}
 else {$$endpoint = "/v1/summarize";}
