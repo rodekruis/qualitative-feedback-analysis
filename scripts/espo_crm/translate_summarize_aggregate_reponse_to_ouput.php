aa719e69b331fcc1d9224895183561f431a7d81a
Cas Teeuwen <casteeuwen@pm.me>
2026-06-16 15:33:07 +0200

    refactor(espo): remove unused espo scripts

--- scripts/espo_crm/translate_summarize_aggregate_reponse_to_ouput.php
+++
@@ -1,7 +0,0 @@
-<?php // Do not copy this tag into EspoCRM
-// Script for translating response to output string for summarize aggregate feature in EspoCRM
-// The QFA backend now returns a pre-formatted pretty_output field on the summary object.
-
-$response = $_lastHttpResponseBody;
-
-$modelResponse = json\retrieve($response, 'summary.pretty_output');
